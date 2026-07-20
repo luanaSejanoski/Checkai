e92350ecf3ee329fa234080b7884a4265ae5b797
luanaSejanoski <131304589+luanaSejanoski@users.noreply.github.com>
2026-07-20 14:03:51 -0300

    cria os primeiros arquivos

---
+++ Checkai/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Checkai.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Checkai.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
---
+++ Checkai/Models/Habito.cs
@@ -0,0 +1,10 @@
+namespace Checkai.Models;
+
+public class Habito
+{
+    public int Id {get; set;}
+
+    public string Nome {get; set;} = string.Empty; //valor inicial é um texto vazio 
+
+    public string Descricao {get; set;} = string.Empty;
+}
