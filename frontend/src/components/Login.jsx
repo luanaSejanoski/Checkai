081eadb1acbc9319875222ce0a7aecca66006236
luanaSejanoski <131304589+luanaSejanoski@users.noreply.github.com>
2026-09-05 15:21:57 -0300

    Adiciona controle dos campos de login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react"
+
 function Login({onLogin}){
+    
+    const[email, setEmail] = useState('');
+    const[senha, setSenha] = useState('');
+    
     return(
         <div>
             <h1>Login</h1>
  
-            <div class= "campo">
-            <input type="email" name="email" id="" /><br />
-            <input type="password" name="senha" id="" />
-            </div>
+            <div className= "campo">
+            <input
+             type="email"
+             name="email" 
+             onChange={(event) => setEmail(event.target.value)}/><br />
 
-            
+            <input
+             type="password"
+             name="senha"
+             onChange={(event) => setSenha(event.target.value)}
+              />
+            </div>
 
             <button onClick={() => onLogin(true)}>Entrar</button>
         </div>
     )
+    
 }
+
 
 export default Login
